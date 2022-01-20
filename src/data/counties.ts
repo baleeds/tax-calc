756de0be9645f7cb2111e8fc32b7e2501a1261c5
Benjamin Leeds <bleeds@samaritan.org>
2022-01-19 20:40:27 -0500

    add: All counties

--- src/data/counties.ts
+++ src/data/counties.ts
@@ -6,13 +6,503 @@
 
 export const counties: County[] = [
   {
-    id: 'buncombe',
-    name: 'Buncombe',
+    id: 'alamance',
+    name: 'Alamance',
+    taxRate: 2,
+  },
+  {
+    id: 'alexander',
+    name: 'Alexander',
+    taxRate: 2.25,
+  },
+  {
+    id: 'alleghany',
+    name: 'Alleghany',
+    taxRate: 2,
+  },
+  {
+    id: 'anson',
+    name: 'Anson',
+    taxRate: 2.25,
+  },
+  {
+    id: 'ashe',
+    name: 'Ashe',
     taxRate: 2.25,
   },
   {
     id: 'avery',
     name: 'Avery',
+    taxRate: 2,
+  },
+  {
+    id: 'beaufort',
+    name: 'Beaufort',
+    taxRate: 2,
+  },
+  {
+    id: 'bertie',
+    name: 'Bertie',
+    taxRate: 2.25,
+  },
+  {
+    id: 'bladen',
+    name: 'Bladen',
+    taxRate: 2,
+  },
+  {
+    id: 'brunswick',
+    name: 'Brunswick',
+    taxRate: 2,
+  },
+  {
+    id: 'buncombe',
+    name: 'Buncombe',
+    taxRate: 2.25,
+  },
+  {
+    id: 'burke',
+    name: 'Burke',
+    taxRate: 2,
+  },
+  {
+    id: 'cabarrus',
+    name: 'Cabarrus',
+    taxRate: 2.25,
+  },
+  {
+    id: 'caldwell',
+    name: 'Caldwell',
+    taxRate: 2,
+  },
+  {
+    id: 'camden',
+    name: 'Camden',
+    taxRate: 2,
+  },
+  {
+    id: 'carteret',
+    name: 'Carteret',
+    taxRate: 2.25,
+  },
+  {
+    id: 'caswell',
+    name: 'Caswell',
+    taxRate: 2,
+  },
+  {
+    id: 'catawba',
+    name: 'Catawba',
+    taxRate: 2.25,
+  },
+  {
+    id: 'chatham',
+    name: 'Chatham',
+    taxRate: 2.25,
+  },
+  {
+    id: 'cherokee',
+    name: 'Cherokee',
+    taxRate: 2.25,
+  },
+  {
+    id: 'chowan',
+    name: 'Chowan',
+    taxRate: 2,
+  },
+  {
+    id: 'clay',
+    name: 'Clay',
+    taxRate: 2.25,
+  },
+  {
+    id: 'cleveland',
+    name: 'Cleveland',
+    taxRate: 2,
+  },
+  {
+    id: 'columbus',
+    name: 'Columbus',
+    taxRate: 2,
+  },
+  {
+    id: 'craven',
+    name: 'Craven',
+    taxRate: 2,
+  },
+  {
+    id: 'cumberland',
+    name: 'Cumberland',
+    taxRate: 2.25,
+  },
+  {
+    id: 'currituck',
+    name: 'Currituck',
+    taxRate: 2,
+  },
+  {
+    id: 'dare',
+    name: 'Dare',
+    taxRate: 2,
+  },
+  {
+    id: 'davidson',
+    name: 'Davidson',
+    taxRate: 2.25,
+  },
+  {
+    id: 'davie',
+    name: 'Davie',
+    taxRate: 2,
+  },
+  {
+    id: 'duplin',
+    name: 'Duplin',
+    taxRate: 2.25,
+  },
+  {
+    id: 'durham',
+    name: 'Durham',
+    taxRate: 2.75,
+  },
+  {
+    id: 'edgecombe',
+    name: 'Edgecombe',
+    taxRate: 2.25,
+  },
+  {
+    id: 'forsyth',
+    name: 'Forsyth',
+    taxRate: 2.25,
+  },
+  {
+    id: 'franklin',
+    name: 'Franklin',
+    taxRate: 2,
+  },
+  {
+    id: 'gaston',
+    name: 'Gaston',
+    taxRate: 2.25,
+  },
+  {
+    id: 'gates',
+    name: 'Gates',
+    taxRate: 2,
+  },
+  {
+    id: 'graham',
+    name: 'Graham',
+    taxRate: 2.25,
+  },
+  {
+    id: 'granville',
+    name: 'Granville',
+    taxRate: 2,
+  },
+  {
+    id: 'greene',
+    name: 'Greene',
+    taxRate: 2.25,
+  },
+  {
+    id: 'guilford',
+    name: 'Guilford',
+    taxRate: 2,
+  },
+  {
+    id: 'halifax',
+    name: 'Halifax',
+    taxRate: 2.25,
+  },
+  {
+    id: 'harnett',
+    name: 'Harnett',
+    taxRate: 2.25,
+  },
+  {
+    id: 'haywood',
+    name: 'Haywood',
+    taxRate: 2.25,
+  },
+  {
+    id: 'henderson',
+    name: 'Henderson',
+    taxRate: 2,
+  },
+  {
+    id: 'hertford',
+    name: 'Hertford',
+    taxRate: 2.25,
+  },
+  {
+    id: 'hoke',
+    name: 'Hoke',
+    taxRate: 2,
+  },
+  {
+    id: 'hyde',
+    name: 'Hyde',
+    taxRate: 2,
+  },
+  {
+    id: 'iredell',
+    name: 'Iredell',
+    taxRate: 2,
+  },
+  {
+    id: 'jackson',
+    name: 'Jackson',
+    taxRate: 2.25,
+  },
+  {
+    id: 'johnston',
+    name: 'Johnston',
+    taxRate: 2,
+  },
+  {
+    id: 'jones',
+    name: 'Jones',
+    taxRate: 2.25,
+  },
+  {
+    id: 'lee',
+    name: 'Lee',
+    taxRate: 2.25,
+  },
+  {
+    id: 'lenoir',
+    name: 'Lenoir',
+    taxRate: 2.25,
+  },
+  {
+    id: 'lincoln',
+    name: 'Lincoln',
+    taxRate: 2.25,
+  },
+  {
+    id: 'macon',
+    name: 'Macon',
+    taxRate: 2,
+  },
+  {
+    id: 'madison',
+    name: 'Madison',
+    taxRate: 2.25,
+  },
+  {
+    id: 'martin',
+    name: 'Martin',
+    taxRate: 2.25,
+  },
+  {
+    id: 'mcdowell',
+    name: 'Mcdowell',
+    taxRate: 2,
+  },
+  {
+    id: 'mecklenburg',
+    name: 'Mecklenburg',
+    taxRate: 2.5,
+  },
+  {
+    id: 'mitchell',
+    name: 'Mitchell',
+    taxRate: 2,
+  },
+  {
+    id: 'montgomery',
+    name: 'Montgomery',
+    taxRate: 2.25,
+  },
+  {
+    id: 'moore',
+    name: 'Moore',
+    taxRate: 2.25,
+  },
+  {
+    id: 'nash',
+    name: 'Nash',
+    taxRate: 2.25,
+  },
+  {
+    id: 'new-hanover',
+    name: 'New Hanover',
+    taxRate: 2.25,
+  },
+  {
+    id: 'northampton',
+    name: 'Northampton',
+    taxRate: 2,
+  },
+  {
+    id: 'onslow',
+    name: 'Onslow',
+    taxRate: 2.25,
+  },
+  {
+    id: 'orange',
+    name: 'Orange',
+    taxRate: 2.75,
+  },
+  {
+    id: 'pamlico',
+    name: 'Pamlico',
+    taxRate: 2,
+  },
+  {
+    id: 'pasquotank',
+    name: 'Pasquotank',
+    taxRate: 2.25,
+  },
+  {
+    id: 'pender',
+    name: 'Pender',
+    taxRate: 2,
+  },
+  {
+    id: 'perquimans',
+    name: 'Perquimans',
+    taxRate: 2,
+  },
+  {
+    id: 'person',
+    name: 'Person',
+    taxRate: 2.75,
+  },
+  {
+    id: 'pitt',
+    name: 'Pitt',
+    taxRate: 2.25,
+  },
+  {
+    id: 'polk',
+    name: 'Polk',
+    taxRate: 2,
+  },
+  {
+    id: 'randolph',
+    name: 'Randolph',
+    taxRate: 2.25,
+  },
+  {
+    id: 'richmond',
+    name: 'Richmond',
+    taxRate: 2,
+  },
+  {
+    id: 'robeson',
+    name: 'Robeson',
+    taxRate: 2.25,
+  },
+  {
+    id: 'rockingham',
+    name: 'Rockingham',
+    taxRate: 2.25,
+  },
+  {
+    id: 'rowan',
+    name: 'Rowan',
+    taxRate: 2.25,
+  },
+  {
+    id: 'rutherford',
+    name: 'Rutherford',
+    taxRate: 2.25,
+  },
+  {
+    id: 'sampson',
+    name: 'Sampson',
+    taxRate: 2.25,
+  },
+  {
+    id: 'scotland',
+    name: 'Scotland',
+    taxRate: 2,
+  },
+  {
+    id: 'stanly',
+    name: 'Stanly',
+    taxRate: 2.25,
+  },
+  {
+    id: 'stokes',
+    name: 'Stokes',
+    taxRate: 2,
+  },
+  {
+    id: 'surry',
+    name: 'Surry',
+    taxRate: 2.25,
+  },
+  {
+    id: 'swain',
+    name: 'Swain',
+    taxRate: 2.25,
+  },
+  {
+    id: 'transylvania',
+    name: 'Transylvania',
+    taxRate: 2,
+  },
+  {
+    id: 'tyrrell',
+    name: 'Tyrrell',
+    taxRate: 2,
+  },
+  {
+    id: 'union',
+    name: 'Union',
+    taxRate: 2,
+  },
+  {
+    id: 'vance',
+    name: 'Vance',
+    taxRate: 2,
+  },
+  {
+    id: 'wake',
+    name: 'Wake',
+    taxRate: 2.5,
+  },
+  {
+    id: 'warren',
+    name: 'Warren',
+    taxRate: 2.25,
+  },
+  {
+    id: 'washington',
+    name: 'Washington',
+    taxRate: 2,
+  },
+  {
+    id: 'watauga',
+    name: 'Watauga',
+    taxRate: 2,
+  },
+  {
+    id: 'wayne',
+    name: 'Wayne',
+    taxRate: 2,
+  },
+  {
+    id: 'wilkes',
+    name: 'Wilkes',
+    taxRate: 2.25,
+  },
+  {
+    id: 'wilson',
+    name: 'Wilson',
+    taxRate: 2.25,
+  },
+  {
+    id: 'yadkin',
+    name: 'Yadkin',
+    taxRate: 2,
+  },
+  {
+    id: 'yancey',
+    name: 'Yancey',
     taxRate: 2,
   },
 ];
--- src/utils/calculateTax.ts
+++ src/utils/calculateTax.ts
@@ -1,5 +1,6 @@
 ﻿import { County } from '../data/counties';
 import { roundToTwo } from './roundToTwo';
+import BigNumber from 'bignumber.js';
 
 export interface TaxInfo {
   total: number;
@@ -8,14 +9,15 @@ export interface TaxInfo {
   foodTax: number;
 }
 
-const STATE_TAX_RATE = 4.75;
-const FOOD_TAX_RATE = 2.0;
+const STATE_TAX_RATE = new BigNumber(4.75);
+const FOOD_TAX_RATE = new BigNumber(2.0);
 
 export function calculateTax(county: County, subtotal: number, foodOnlySubtotal: number): TaxInfo {
   const nonFoodSubtotal = subtotal - foodOnlySubtotal;
+
   const countyTax = roundToTwo((nonFoodSubtotal / 100) * county.taxRate);
-  const stateTax = roundToTwo((nonFoodSubtotal / 100) * STATE_TAX_RATE);
-  const foodTax = roundToTwo((foodOnlySubtotal / 100) * FOOD_TAX_RATE);
+  const stateTax = roundToTwo((nonFoodSubtotal / 100) * STATE_TAX_RATE.toNumber());
+  const foodTax = roundToTwo((foodOnlySubtotal / 100) * FOOD_TAX_RATE.toNumber());
   const total = roundToTwo(subtotal + stateTax + foodTax + countyTax);
 
   return {
@@ -27,18 +29,32 @@ export function calculateTax(county: County, subtotal: number, foodOnlySubtotal:
 }
 
 export function splitCountyAndStateTax(taxAmount: number, county: County): { countyTax: number; stateTax: number } {
-  const totalTaxRate = STATE_TAX_RATE + county.taxRate;
+  const countyTaxRate = new BigNumber(county.taxRate);
+  const totalTaxRate = STATE_TAX_RATE.plus(countyTaxRate);
 
-  const countyTax = roundToTwo((taxAmount / totalTaxRate) * county.taxRate);
-  const stateTax = roundToTwo((taxAmount / totalTaxRate) * STATE_TAX_RATE);
+  const countyTax = new BigNumber(taxAmount)
+    .dividedBy(totalTaxRate)
+    .multipliedBy(countyTaxRate)
+    .decimalPlaces(2)
+    .toNumber();
+
+  const stateTax = new BigNumber(taxAmount)
+    .dividedBy(totalTaxRate)
+    .multipliedBy(STATE_TAX_RATE)
+    .decimalPlaces(2)
+    .toNumber();
 
   return { countyTax, stateTax };
 }
 
 export function getTotalForTaxAmounts(nonFoodTax: number, foodTax: number, county: County): number {
-  const nonFoodTaxRate = STATE_TAX_RATE + county.taxRate;
-  const nonFoodSubTotal = nonFoodTax / (nonFoodTaxRate / 100);
-  const foodSubTotal = foodTax / (FOOD_TAX_RATE / 100);
+  const countyTaxRate = new BigNumber(county.taxRate);
+  const nonFoodTaxNumber = new BigNumber(nonFoodTax);
+  const foodTaxNumber = new BigNumber(foodTax);
 
-  return roundToTwo(nonFoodSubTotal + nonFoodTax + foodSubTotal + foodTax);
+  const nonFoodTaxRate = STATE_TAX_RATE.plus(countyTaxRate);
+  const nonFoodSubTotal = nonFoodTaxNumber.dividedBy(nonFoodTaxRate.dividedBy(100));
+  const foodSubTotal = foodTaxNumber.dividedBy(FOOD_TAX_RATE.dividedBy(100));
+
+  return nonFoodSubTotal.plus(nonFoodTaxNumber).plus(foodSubTotal).plus(foodTaxNumber).decimalPlaces(2).toNumber();
 }
